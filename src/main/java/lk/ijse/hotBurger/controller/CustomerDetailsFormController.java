ac941582c5b31b47efb500787fe9ce0cef2c817a
LRLayan <rameshlayan4@gmail.com>
2023-11-06 17:34:20 +0530

    commit.java

---
+++ src/main/java/lk/ijse/hotBurger/controller/CustomerDetailsFormController.java
@@ -0,0 +1,40 @@
+package lk.ijse.hotBurger.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CustomerDetailsFormController {
+
+    @FXML
+    private TableColumn<?, ?> addressLine01;
+
+    @FXML
+    private TableColumn<?, ?> addressLine02;
+
+    @FXML
+    private TableColumn<?, ?> addressLine03;
+
+    @FXML
+    private TableColumn<?, ?> customerId;
+
+    @FXML
+    private TableColumn<?, ?> customerId1;
+
+    @FXML
+    private TableView<?> customerTable;
+
+    @FXML
+    private TableColumn<?, ?> firstName;
+
+    @FXML
+    private TableColumn<?, ?> lastName;
+
+    @FXML
+    private TableColumn<?, ?> phone1;
+
+    @FXML
+    private TableColumn<?, ?> phone2;
+
+    
+}
